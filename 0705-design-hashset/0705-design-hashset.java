15fefba442274a0633b5aacc9562d8f0e7b202b8
Lynn <zzzzzzzl@tamu.edu>
2026-01-06 17:33:34 -0600

    Time: 14 ms (58.11%), Space: 51 MB (87.95%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,69 @@
+class MyHashSet {
+    private static final int BASE = 769;
+    private Node[] buckets;
+
+    private class Node{
+        int key;
+        Node next;
+        Node(int key){
+            this.key=key;
+        }
+    }
+
+    private int hash(int key){
+        return key % BASE;
+    }
+
+    public MyHashSet() {
+        buckets = new Node[BASE];
+        for(int i = 0; i < BASE; i++){
+            buckets[i] = new Node(-1); 
+        }
+    }
+    
+    public void add(int key) {
+        // 计算index，检查是否有重复，挂在最后面
+        int idx = hash(key);
+        Node cur = buckets[idx];
+        while(cur.next!=null){
+            cur=cur.next;
+            if(cur.key==key){
+                return;
+            }
+        }
+        cur.next = new Node(key);
+    }
+    
+    public void remove(int key) {
+        // 查找key的前一个节点，移除
+        int idx = hash(key);
+        Node cur = buckets[idx];
+        while(cur.next!=null){
+            if(cur.next.key == key){
+                cur.next = cur.next.next;                
+                return;
+            }
+            cur=cur.next;
+        }
+    }
+    
+    public boolean contains(int key) {
+        int idx = hash(key);
+        Node cur = buckets[idx];
+        while(cur.next!=null){
+            cur=cur.next;
+            if(cur.key==key){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
